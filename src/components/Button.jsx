03492e277511ec22a4521e68daeae518e3b5e0eb
Abhinav290704 <gaurabhinav472@gmail.com>
2025-12-05 14:29:29 +0530

    scrollbar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import CodeEditor from "../components/CodeEditor"
 const Button = () => {
     return(
       <>
-    <div className="min-h-screen bg-sky-100 flex flex-col">
+    <div className=" bg-sky-100 flex flex-col">
       <div className="flex space-x-4 px-3  items-center h-11 bg-sky-100 shadow-sm">
 <button
   className="
