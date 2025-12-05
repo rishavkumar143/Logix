9be8bd4b524c02d16bce8e527b6087d8cdc10dbd
rishavkumar143 <rishavkumar4211@gmail.com>
2025-12-05 17:22:14 +0530

    icons updated

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -60,7 +60,7 @@ const CodeEditor = () => {
   const lines = code.split("\n");
 
   return (
-    <div className="w-full bg-[#1B1B1B] text-gray-200 p-1 h-[705px] border border-gray-700">
+    <div className="w-full bg-[#1B1B1B] text-gray-200 p-1 h-screen border border-gray-700">
       <div className="h-full overflow-y-auto scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-slate-800 flex text-sm font-mono">
         <div className="pr-4 text-[#569CD6] bg-slate-800/40 select-none text-right border-r border-slate-700">
           {lines.map((_, i) => (
